f2da5de243586cc43b6c36b71d78c2d432e846e0
Zain Rizwan Khan <zainrizwankhan@gmail.com>
2023-01-03 23:55:49 +0500

    Created common run_test function

---
+++ examples/tutorial/run_test.py
@@ -0,0 +1,36 @@
+import cocotb
+
+from cocotb.clock import Clock
+
+from EndianSwapperTB import *
+
+@cocotb.coroutine
+def run_test(dut, data_in=None, config_coroutine=None, 
+                idle_inserter=None, 
+                backpressure_inserter=None):
+
+    cocotb.fork(Clock(dut.clk, 5000).start())
+    tb = EndianSwapperTB(dut)
+
+    yield tb.reset()
+    dut.stream_out_ready <= 1
+
+    # Start off any optional coroutines
+    if config_coroutine is not None:
+        cocotb.fork(config_coroutine(tb.csr))
+    if idle_inserter is not None:
+        tb.stream_in.set_valid_generator(idle_inserter())
+    if backpressure_inserter is not None:
+        tb.backpressure.start(backpressure_inserter())
+
+    # Send in the packets
+    for transaction in data_in():
+        yield tb.stream_in.send(transaction)
+
+    # Wait at least 2 cycles where output ready is low before ending the test
+    for i in range(2):
+        yield RisingEdge(dut.clk)
+        while not dut.stream_out_ready.value:
+            yield RisingEdge(dut.clk)
+
+    pkt_count = yield tb.csr.read(1)
