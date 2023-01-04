4b49ad9b67ef7b48e890d5c208b059256e6df369
Zain Rizwan Khan <zainrizwankhan@gmail.com>
2023-01-04 16:49:32 +0500

    Moved factory code to testbench top

--- examples/tutorial/tb_top.py
+++ examples/tutorial/tb_top.py
@@ -1,11 +1,28 @@
+import warnings
+
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
+from cocotb.regression import TestFactory
 
 from EndianSwapperTB import *
 
 import cocotb.wavedrom
 
-from factory import *
+from run_test import *
+from argument_lib import *
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from cocotb.generators.bit import wave, intermittent_single_cycles, random_50_percent
+
+factory = TestFactory(run_test)
+
+factory.add_option("data_in",               [random_packet_sizes])
+factory.add_option("config_coroutine",      [None, randomly_switch_config])
+factory.add_option("idle_inserter",         [None, wave, intermittent_single_cycles, random_50_percent])
+factory.add_option("backpressure_inserter", [None, wave, intermittent_single_cycles, random_50_percent])
+
+factory.generate_tests()
 
 @cocotb.test()
 async def wavedrom_test(dut):
