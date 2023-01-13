841ae9e7718e6dd23054bf97d13b2413b622c6a9
Zain Rizwan Khan <zainrizwankhan@gmail.com>
2023-01-13 23:22:41 +0500

    Added instances for memory top level modules

--- verilog/Core_top.sv
+++ verilog/Core_top.sv
@@ -4,24 +4,37 @@ module Core_top (
     output logic [31:0] io_pin
 );
 
+    logic        data_req;
+    logic [31:0] data_addr;
+    logic [31:0] data_wdata;
+    logic [ 3:0] data_be;
+    logic        data_we;
+    logic        data_valid;
+    logic [31:0] data_rdata;
+
+    logic        instr_req;
+    logic [31:0] instr_addr;
+    logic        instr_valid;
+    logic [31:0] instr_rdata; 
+
     Core u_nucleusrv_core (
         // Clock and reset
         .clock  ( clock  ),
         .reset  ( reset  ),
         .io_pin ( io_pin ),
         // Data memory interface
-        .io_dmemReq_valid               (  ),
-        .io_dmemReq_bits_addrRequest    (  ),
-        .io_dmemReq_bits_dataRequest    (  ),
-        .io_dmemReq_bits_activeByteLane (  ),
-        .io_dmemReq_bits_isWrite        (  ),
-        .io_dmemRsp_valid               (  ),
-        .io_dmemRsp_bits_dataResponse   (  ),
+        .io_dmemReq_valid               ( data_req   ),
+        .io_dmemReq_bits_addrRequest    ( data_addr  ),
+        .io_dmemReq_bits_dataRequest    ( data_wdata ),
+        .io_dmemReq_bits_activeByteLane ( data_be    ),
+        .io_dmemReq_bits_isWrite        ( data_we    ),
+        .io_dmemRsp_valid               ( data_valid ),
+        .io_dmemRsp_bits_dataResponse   ( data_rdata ),
         // Instruction memory interface
-        .io_imemReq_valid             (  ),        
-        .io_imemReq_bits_addrRequest  (  ),
-        .io_imemRsp_valid             (  ),
-        .io_imemRsp_bits_dataResponse (  ),
+        .io_imemReq_valid             ( instr_req   ),        
+        .io_imemReq_bits_addrRequest  ( instr_addr  ),
+        .io_imemRsp_valid             ( instr_valid ),
+        .io_imemRsp_bits_dataResponse ( instr_rdata ),
         // RVFI
         .io_rvfiUInt_0    (  ),
         .io_rvfiUInt_1    (  ),
@@ -38,4 +51,28 @@ module Core_top (
         .io_rvfiRegAddr_2 (  )
     );
 
+    SRamTop dmem (
+        // Clock and reset
+        .clock ( clock ),
+        .reset ( reset ),
+        // Memory interface
+        .io_req_valid               ( data_req   ),
+        .io_req_bits_addrRequest    ( data_addr  ),
+        .io_req_bits_dataRequest    ( data_wdata ),
+        .io_req_bits_activeByteLane ( data_be    ),
+        .io_req_bits_isWrite        ( data_we    ).
+        .io_rsp_valid               ( data_valid ),
+        .io_rsp_bits_dataResponse   ( data_rdata )
+    );
+
+    SRamTop_1 imem (
+        // Clock and reset
+        .clock ( clock ),
+        .reset ( reset ),
+        // Memory interface
+        .io_req_valid             ( instr_req   ),
+        .io_req_bits_addrRequest  ( instr_addr  ),
+        .io_rsp_valid             ( instr_valid ),
+        .io_rsp_bits_dataResponse ( instr_rdata )
+    );
 endmodule
